46ab1de29dd4667a6d716919e2c2793385de4c6d
Alan <alantessier97@gmail.com>
2021-01-14 12:11:29 -0500

    tuned parameters

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
         name="gtrxl-torch",
-        version = '0.1.3',
+        version = '0.1.5',
         license="MIT", 
         description = "Gated-Transformer XL - PyTorch",
         author = "Alan Tessier",
